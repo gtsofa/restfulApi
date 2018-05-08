22290042e365cc59bc1e6b6d64dfe47c5559f436
gtsofa <jlsnyule@yahoo.com>
2018-05-08 23:54:12 +0300

    refactor

--- restful.py
+++ restful.py
@@ -16,6 +16,13 @@ def test():
 def returnAll():
     return jsonify({'languages': languages})
 
+@app.route('/lang/<string:name>', methods=['GET'])
+def returnOne(name):
+    #use lamba to search through the list
+    langs = [language for language in languages if language['name'] == name]
+    return jsonify({'language': langs[0]})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
